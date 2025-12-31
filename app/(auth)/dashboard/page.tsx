75ce57e354ba251ca9610ad83e7e662c8bc194f4
Dipen <lamadipen9953@gmail.com>
2025-12-31 07:19:23 +0545

    dashboard done

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { LayoutDashboard, ShoppingBag, History, Settings, LogOut} from 'lucide-react';
 
 const DashboardPage = () => {
+  
   return (
     <div className="flex min-h-screen bg-[#000000] text-white font-sans">          
           <h1 className="text-5xl font-extrabold tracking-tighter mb-4 text-white">
