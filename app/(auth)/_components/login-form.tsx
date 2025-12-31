363212f05f1b7b74103b60066a3c8b6ebbdfd303
Dipen <lamadipen9953@gmail.com>
2025-12-31 07:13:48 +0545

    login page done

--- app/(auth)/_components/login-form.tsx
+++ app/(auth)/_components/login-form.tsx
@@ -1,12 +1,13 @@
 export default function LoginForm() {
     return (
-        
+
         <div className="mx-auto max-w-md p-4 border rounded">
             Login Header  
             <div>
                 <label>Email</label>
                 <input type="emial"/>
             </div>
+            
             <div>
                 <label>Password</label>
                 <input type="password"/>
